73fe2d1d99f3ef6e534d8efc91fc7878971e15e9
abwqr <ab1502144@gmail.com>
2023-05-02 16:29:44 +0500

    first commit

---
+++ wiki.go
@@ -0,0 +1,18 @@
+package main
+
+// import "fmt"
+
+import (
+	"fmt"
+	// "log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+}
